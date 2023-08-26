c2d7841b992dc4a462313e43748b410e7df4027d
Mosa Akhi Akhter <143069943+Mosa-Akhi-Akhter@users.noreply.github.com>
2023-08-26 17:32:04 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# E-Commerce-Website-
-Welcome to our open-source E-commerce Website project powered by Python and Django. Our mission is to provide a versatile and customizable platform for online retailers to set up their digital storefronts, manage products, and serve customers seamlessly.
+"# Django_full_Ecom_App" 
---
+++ models.py
@@ -0,0 +1,121 @@
+from django.db import models
+
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.fields import RichTextUploadingField
+# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+from django.contrib.auth.models import User
+
+from django.forms import ModelForm
+from django.db.models import Count, Sum, Avg
+
+
+class Category(MPTTModel):
+    status = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    parent = TreeForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    title = models.CharField(max_length=200)
+    keywords = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, upload_to='category/')
+    details = models.TextField()
+    status = models.CharField(max_length=20, choices=status)
+    slug = models.SlugField(null=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    status = (
+        ('True', 'True'),
+        ('False', 'False'),)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    keywords = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, upload_to='product/')
+    new_price = models.DecimalField(decimal_places=2, max_digits=15, default=0)
+    old_price = models.DecimalField(decimal_places=2, max_digits=15)
+    amount = models.IntegerField(default=0)
+    min_amount = models.IntegerField(default=3)
+    detail = models.TextField()
+    status = models.CharField(max_length=20, choices=status)
+    slug = models.SlugField(null=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def ImageUrl(self):
+        if self.image:
+            return self.image.url
+        else:
+            return ""
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" heights="70" width="60" />'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+    def average_review(self):
+        reviews = Comment.objects.filter(
+            product=self, status=True).aggregate(average=Avg('rate'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+            return avg
+        else:
+            return avg
+
+    def total_review(self):
+        reviews = Comment.objects.filter(
+            product=self, status=True).aggregate(count=Count('id'))
+        cnt = 0
+        if reviews['count'] is not None:
+            cnt = (reviews['count'])
+            return cnt
+
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(blank=True, upload_to='product/')
+
+    def __str(self):
+        return self.title
+
+
+class Comment(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('True', 'True'),
+        ('False', 'False'),
+
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=200, blank=True)
+    comment = models.CharField(max_length=500, blank=True)
+    rate = models.IntegerField(default=1)
+    ip = models.CharField(max_length=100, blank=True)
+    status = models.CharField(max_length=40, choices=STATUS, default='New')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class CommenttForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['subject', 'comment', 'rate']
